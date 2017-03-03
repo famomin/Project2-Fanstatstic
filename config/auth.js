c0265acd140305bfeb882c2b6aca6682b9e6ff7e
Anthony Myhre <anthony.myhre@utexas.edu>
2017-03-03 15:06:02 -0600

    pushing to heroku

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '',
-		'clientSecret': '',
+		'clientID': '397065384003010',
+        'clientSecret': '3a83c16253431b0be8029ccec315febe',
 		'callbackURL': 'http://localhost:3000/login/facebook/return'
 	},
 
 	'googleAuth' : {
-		'clientID': '',
-		'clientSecret': '',
+		'clientID': '369124883170-1v8qm45nkl893td08i1f6nr1lqlkkml0.apps.googleusercontent.com',
+        'clientSecret': 'qvgrAh7g2ZRnOWeWWiJFSzzt',
 		'callbackURL': 'http://localhost:3000/auth/google/callback'
 	}
 }
