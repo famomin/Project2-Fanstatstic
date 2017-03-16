5bffccb14a9c19cc896bc3d8d11b70b3448e7c48
Anthony Myhre <anthony.myhre@utexas.edu>
2017-03-15 21:25:20 -0500

    Added comments to model_controller.js Changed test to model and testDB to results

--- controllers/model_controller.js
+++ controllers/model_controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var test = require('./../models');
+// model is the an object of all of the functions exported from the model files
+var model = require('./../models');
 
 /*
     routes needed
         get route for 
 */
-module.exports = function(app) {
-	app.get('/search', function(req, res) {
-        // test.Player_team.findAll({
-        console.log(req.query);
-        test.Player.findAll({
+module.exports = function (app) {
+    // get route that searches for players from the players table using Twitter Typeahead
+    // each key stroke triggers a query that searches for records that have a first name, last name, or position like what the user has typed
+    // the query does a tripple join on the players, player_teams, and teams tables
+    app.get('/search', function (req, res) {
+        model.Player.findAll({
             where: {
                 $or: {
                     first_name: {
@@ -25,36 +27,33 @@ module.exports = function(app) {
                 }
             }, include: [
                 {
-                    model: test.Player_team,
+                    model: model.Player_team,
                     as: 'Player_team',
                     include: [
                         {
-                            model: test.Team
+                            model: model.Team
                         }
                     ]
                 }
-            ]
-        }).then(function(testDB) {
+            ] // end of include array
+        }).then(function (results) {
+            // when the query is done, run this callback function and pass in results as the parameter
+            // results is an array filled with the records the query returned
+            // create an empty array to hold the data we want
+            // loop through results and push the dataValues which contains the player's info
             var player_team = [];
-            for (var i = 0; i < testDB.length; i++) {
-                var player = {
-                    player_team
-                }
-                player_team.push(testDB[i].dataValues);
+            for (var i = 0; i < results.length; i++) {
+                player_team.push(results[i].dataValues);
             }
-            console.log("\n\nPlayer_team\n\n");
-            console.log(player_team[0]);
-            res.end(JSON.stringify(testDB));
-            /*console.log(player_team[0].Player_team[0].dataValues);
-            console.log(player_team[0].Player_team[0].dataValues.Team.dataValues);*/
-        });
+            // return a stringified version of the array back to the ajax call
+            res.end(JSON.stringify(results));
+        }); // end of .then promise
+    }); // end of app.get('/search')
 
-	});
-    
-    app.post('/displayComparison', function(req, res) {
-        console.log(req.body);
-
-         test.Player.findAll({
+    // post route that runs a query does a triple join on the players table, player_teams and player_stats
+    // this query returns the stats of the players that the user selected to be displayed
+    app.post('/displayComparison', function (req, res) {
+        model.Player.findAll({
             where: {
                 id: {
                     $in: req.body.playerID.split(",")
@@ -62,38 +61,33 @@ module.exports = function(app) {
             },
             include: [
                 {
-                    model: test.Player_team,
+                    model: model.Player_team,
                     as: 'Player_team',
                     include: [
                         {
-                            model: test.Team
+                            model: model.Team
                         }
                     ]
                 },
                 {
-                    model: test.Player_stat,
+                    model: model.Player_stat,
                     as: 'Player_stat'
                 }
-            ]
-        }).then(function(testDB) {
+            ] // end of include array
+        }).then(function (results) {
+            // create an empty array to hold the new player objects we create in the for loop
+            // loop through results and create an object with 'FirstName LastName PlayerTeam PlayerPosition' string and the player's stats as properties
+            // push the object to the players array
             var players = [];
-            for (var i = 0; i < testDB.length; i++) {
+            for (var i = 0; i < results.length; i++) {
                 var newPlayer = {
-                    playerInfo: testDB[i].dataValues.first_name + " " + testDB[i].dataValues.last_name + " " + testDB[i].Player_team[0].Team.team_abbr + " " + testDB[i].player_position,
-                    stats: testDB[i].Player_stat[0].dataValues
+                    playerInfo: results[i].dataValues.first_name + " " + results[i].dataValues.last_name + " " + results[i].Player_team[0].Team.team_abbr + " " + results[i].player_position,
+                    stats: results[i].Player_stat[0].dataValues
                 };
-
                 players.push(newPlayer);
             };
-            console.log(players);
-            /*console.log(testDB[0].dataValues);
-            console.log();
-            console.log(testDB[0].Player_team[0].dataValues);
-            console.log();
-            console.log(testDB[0].Player_team[0].dataValues.Team);
-            console.log();
-            console.log(testDB[0].Player_stat[0].dataValues);*/
-            res.render('playerstats', {Player: players});
-        });
-    });
-};
+            // render the players array to playerstats.handlebars
+            res.render('playerstats', { Player: players });
+        }); // end of .then promise
+    }); // end of app.post('displayComparison')
+}; // end of module.exports
